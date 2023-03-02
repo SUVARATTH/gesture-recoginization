3b780c75e94fe97eb0436a2e1351214c84ba9b70
SUVARATTH <123358858+SUVARATTH@users.noreply.github.com>
2023-03-02 22:36:03 +0530

    Add files via upload

---
+++ gesture/sign.py.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
+face=mp.solutions.face_mesh
+ model = load_model('mp_hand_gesture')
+f = open('gesture.names', 'r')
+classNames = f.read().split('\n')
+f.close()
+print(classNames)
+cap = cv2.VideoCapture(0)
+kill=[]
+while True:
+    _, frame = cap.read()
+    x, y, c = frame.shape
+    frame = cv2.flip(frame, 1)
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(framergb)
+    className = ''
+    
+    if result.multi_hand_landmarks:
+        landmarks = []
+        for handslms in result.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
+
+                landmarks.append([lmx, lmy])
+            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+            
+            prediction = model.predict([landmarks])
+            classID = np.argmax(prediction)
+            className = classNames[classID]
+            kill.append(classNames[classID])
+    
+    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (0,0,255), 2, cv2.LINE_AA)
+    cv2.imshow("Output", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+
+cv2.destroyAllWindows()
